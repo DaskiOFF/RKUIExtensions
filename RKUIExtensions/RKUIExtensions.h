40b72edbdcb568654c152515bbaaed407a48f431
Roman Kotov <waydeveloper@gmail.com>
2018-06-10 21:31:51 +0300

    Initial Commit

---
+++ RKUIExtensions/RKUIExtensions.h
@@ -0,0 +1,19 @@
+//
+//  RKUIExtensions.h
+//  RKUIExtensions
+//
+//  Created by Roman Kotov on 10/06/2018.
+//  Copyright © 2018 Roman Kotov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RKUIExtensions.
+FOUNDATION_EXPORT double RKUIExtensionsVersionNumber;
+
+//! Project version string for RKUIExtensions.
+FOUNDATION_EXPORT const unsigned char RKUIExtensionsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RKUIExtensions/PublicHeader.h>
+
+
