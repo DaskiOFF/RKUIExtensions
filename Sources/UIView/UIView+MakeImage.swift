8de7463097443970d0adf8265e2bd99080a73cc0
Roman Kotov <waydeveloper@gmail.com>
2018-10-06 00:17:25 +0300

    [UIView] Add makeImage

---
+++ Sources/UIView/UIView+MakeImage.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+extension UIView {
+    func makeImage() -> UIImage? {
+        UIGraphicsBeginImageContextWithOptions(bounds.size, isOpaque, UIScreen.main.scale)
+        defer { UIGraphicsEndImageContext() }
+
+        guard let context = UIGraphicsGetCurrentContext() else { return nil }
+
+        layer.render(in: context)
+        let image = UIGraphicsGetImageFromCurrentImageContext()
+        return image
+    }
+}
