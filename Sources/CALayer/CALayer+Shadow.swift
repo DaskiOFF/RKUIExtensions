3ffad8181895d669f1feaf5baad7000653c03a6a
Roman Kotov <waydeveloper@gmail.com>
2018-10-06 22:32:14 +0300

    Fix 'public'

--- Sources/CALayer/CALayer+Shadow.swift
+++ Sources/CALayer/CALayer+Shadow.swift
@@ -1,7 +1,7 @@
 import UIKit
 
 extension CALayer {
-    func shadow(withColor color: UIColor, x: CGFloat, y: CGFloat, blur: CGFloat, opacity: Float = 1) {
+    public func shadow(withColor color: UIColor, x: CGFloat, y: CGFloat, blur: CGFloat, opacity: Float = 1) {
         shadowColor = color.cgColor
         shadowOpacity = opacity
         shadowOffset = CGSize(width: x, height: y)
