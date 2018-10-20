4eace652ff020de896a72104176d9121ad15e5f5
Roman Kotov <waydeveloper@gmail.com>
2018-10-20 14:40:27 +0300

    No message

--- Sources/UIViewController/UIViewController+iOS9ScrollView.swift
+++ Sources/UIViewController/UIViewController+iOS9ScrollView.swift
@@ -2,9 +2,11 @@ import UIKit
 
 extension UIViewController {
     public func adjustsScrollViewInsetsFalseIfNeeded() {
-        if #available(iOS 10, *) {
+        if #available(iOS 11, *) {
         } else {
             adjustsScrollViewInsets(false)
+            extendedLayoutIncludesOpaqueBars = false
+            automaticallyAdjustsScrollViewInsets = false
         }
     }
 }
